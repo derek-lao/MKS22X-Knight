db60bdef6501ffabdce3beb474ddd8788cf1e38e
Derek Lao <dlao00@stuy.edu>
2019-02-18 03:14:08 -0500

    wrote countHelper method, extremely similar to the solveHelper method. Implemented it into the wrapper funciton. Created a field called 'count' to count solutions. At the start of countSolutions, I will set coutn to 0. I have yet to add a toString() method

--- KnightBoard.java
+++ KnightBoard.java
@@ -15,10 +15,12 @@ public class KnightBoard{
         board[r][c] = 0;
       }
     }
-    this.rows = rows;
-    this.cols = cols;
+    this.rows = rows+1;
+    this.cols = cols+1;
     squares = this.rows * this.cols;
   }
+
+  private int count;
 
   private boolean addable(int row,int col){
     return board[row][col] == 0;
@@ -29,7 +31,16 @@ public class KnightBoard{
   private boolean outOfBounds(int row,int col){
     return(row < 0 || row >= board.length || col < 0 || col >= board[0].length);
   }
-
+  private boolean illegalBoard(){
+    for(int r=0; r<rows; r++)
+    {
+      for(int c=0; c<cols; c++)
+      {
+        if(board[r][c]!=0) return false;
+      }
+    }
+    return true;
+  }
   private boolean solveHelper(int label,int row,int col){
     if(outOfBounds(row,col)) return false;
     else
@@ -65,10 +76,41 @@ public class KnightBoard{
   @return true when the board is solvable from the specified starting position
   */
   public boolean solve(int startingRow,int startingCol){
+    if(illegalBoard())
+    {
+      throw new IllegalStateException();
+    }
+    if(outOfBounds(startingRow,startingCol))
+    {
+      throw new IllegalArgumentException();
+    }
     return solveHelper(1,startingRow,startingCol);
   }
 
-
+  private void countHelper(int label,int row,int col){
+    if(!outOfBounds(row,col))
+    {
+      if(label >= squares)
+      {
+        count++;
+      }
+      else
+      {
+        if(addable(row,col))
+        {
+          board[row][col] = label;
+          countHelper(label+1,row+2,col+1);
+          countHelper(label+1,row+2,col-1);
+          countHelper(label+1,row+1,col+2);
+          countHelper(label+1,row+1,col-2);
+          countHelper(label+1,row-2,col+1);
+          countHelper(label+1,row-2,col-1);
+          countHelper(label+1,row-1,col+2);
+          countHelper(label+1,row-1,col-1);
+        }
+      }
+    }
+  }
 
 
   /**
@@ -78,6 +120,16 @@ public class KnightBoard{
   @return the number of solutions from the starting position specified
   */
   public int countSolutions(int startingRow, int startingCol){
-
+    if(illegalBoard())
+    {
+      throw new IllegalStateException();
+    }
+    if(outOfBounds(startingRow,startingCol))
+    {
+      throw new IllegalArgumentException();
+    }
+    count=0;
+    countHelper(1,startingRow,startingCol);
+    return count;
   }
 }
