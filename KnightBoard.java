b401fba486fbe72b7106045c01061966c0bf6aa1
Derek Lao <dlao00@stuy.edu>
2019-02-18 02:52:21 -0500

    added fields: rows, cols, and squares, and implemented them in constructor. added private helper methods to return booleans to check if a square is addable and removeable, and to check if a square is outOfBounds (mainly to prevent ArrayIndexOutOfBounds exception). Within the solveHelper method, I am trying to employ the same logic as the Suqeen workout at stuy. But I believe I am close to finishing the website today, but he's amazing though.

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,16 +1,75 @@
 public class KnightBoard{
   private int[][] board;
+  private int rows;
+  private int cols;
+  private int squares;
   /**@throws IllegalArgumentException when either parameter is <= 0.
     */
   // Initialize the board to the correct size and make them all 0's
   public KnightBoard(int rows,int cols){
     board=new int[rows][cols];
-    for(int r=0;r<rows;r++)
+    for(int r=0; r<rows; r++)
     {
-      for(int c=0;c<cols;c++)
+      for(int c=0; c<cols; c++)
       {
-        board[r][c]=0;
+        board[r][c] = 0;
       }
     }
+    this.rows = rows;
+    this.cols = cols;
+    squares = this.rows * this.cols;
+  }
+
+  private boolean addable(int row,int col){
+    return board[row][col] == 0;
+  }
+  private boolean removeable(int row,int col){
+    return !addable(row,col);
+  }
+  private boolean outOfBounds(int row,int col){
+    return(row < 0 || row >= board.length || col < 0 || col >= board[0].length);
+  }
+
+  private boolean solveHelper(int label,int row,int col){
+    if(label >= squares && !outOfBounds(row,col))
+    {
+      return true;
+    }
+    else
+    {
+      if(!outOfBounds(row,col) && addable(row,col))
+      {
+        board[row][col] = label;
+        return
+        solveHelper(label+1,row+2,col+1) ||
+        solveHelper(label+1,row+2,col-1) ||
+        solveHelper(label+1,row+1,col+2) ||
+        solveHelper(label+1,row+1,col-2) ||
+        solveHelper(label+1,row-2,col+1) ||
+        solveHelper(label+1,row-2,col-1) ||
+        solveHelper(label+1,row-1,col+2) ||
+        solveHelper(label+1,row-1,col-1);
+      }
+    }
+  }
+
+  /** Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up to the area of the board in proper knight move steps.
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+   or out of bounds.
+  @return true when the board is solvable from the specified starting position
+  */
+  public boolean solve(int startingRow,int startingCol){
+
+  }
+
+  /**
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+  or out of bounds.
+  @return the number of solutions from the starting position specified
+  */
+  public int countSolutions(int startingRow, int startingCol){
+
   }
 }
