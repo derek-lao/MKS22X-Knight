dd365231fd1468faece1114fa430429e9c48f8da
Derek Lao <dlao00@stuy.edu>
2019-02-18 00:07:34 -0500

    added code for the constructor in KnightBoard

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,16 @@
 public class KnightBoard{
-  
+  private int[][] board;
+  /**@throws IllegalArgumentException when either parameter is <= 0.
+    */
+  // Initialize the board to the correct size and make them all 0's
+  public KnightBoard(int rows,int cols){
+    board=new int[rows][cols];
+    for(int r=0;r<rows;r++)
+    {
+      for(int c=0;c<cols;c++)
+      {
+        board[r][c]=0;
+      }
+    }
+  }
 }
